f28e1dff4f9210930d5d546660f1c4eecc8fd1e5
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-24 21:35:30 +0300

    speed manipulation tests

--- test/train_spec.rb
+++ test/train_spec.rb
@@ -1,43 +1,59 @@
 require 'rspec'
 require_relative '../Train.rb'
 
+
+# There is so much more to do with this tests, but for training purposes its enough
 describe Train do
-  it 'should create object with custom train number' do
-    train = Train.new('cargo', 10, 'some_name')
-    expect(train.train_number).to eq('some_name')
+
+  context '#initialize' do
+    it 'should create object with custom train number' do
+      train = Train.new('cargo', 10, 'some_name')
+      expect(train.train_number).to eq('some_name')
+    end
+    it 'should create random name if no one was provided' do
+      train_1 = Train.new('cargo', 10)
+      train_2 = Train.new('cargo', 10)
+      expect(train_1.train_number).not_to eq(train_2.train_number)
+    end
+    it 'shouldn`t create Trains with non Integer number of carriages' do
+      expect { train = Train.new('cargo', '10') }.to raise_error(ArgumentError)
+      expect { train = Train.new('cargo', ['10']) }.to raise_error(ArgumentError)
+      expect { train = Train.new('cargo', {number: 10}) }.to raise_error(ArgumentError)
+      expect { train = Train.new('cargo', 10.1) }.to raise_error(ArgumentError)
+    end
+    it 'shouldn`t create Trains with number of carriages less than zero' do
+      expect { train = Train.new('passenger', -4) }.to raise_error(ArgumentError)
+    end
+    it 'should create only Trains of cargo and passenger types' do
+      expect { train = Train.new('passenger', 10) }.not_to raise_error
+      expect { train = Train.new('cargo', 10) }.not_to raise_error
+      expect { train = Train.new('Passenger', 10) }.to raise_error(ArgumentError)
+      expect { train = Train.new(10, 10) }.to raise_error(ArgumentError)
+      expect { train = Train.new('personal', 10) }.to raise_error(ArgumentError)
+      expect { train = Train.new(nil, 10) }.to raise_error(ArgumentError)
+    end
   end
-  it 'should create random name if no one was provided' do
-    train_1 = Train.new('cargo', 10)
-    train_2 = Train.new('cargo', 10)
-    expect(train_1.train_number).not_to eq(train_2.train_number)
-  end
-  it 'shouldn`t create Trains with non Integer number of carriages' do
-    expect { train = Train.new('cargo', '10') }.to raise_error(ArgumentError)
-    expect { train = Train.new('cargo', ['10']) }.to raise_error(ArgumentError)
-    expect { train = Train.new('cargo', {number: 10}) }.to raise_error(ArgumentError)
-    expect { train = Train.new('cargo', 10.1) }.to raise_error(ArgumentError)
-  end
-  it 'shouldn`t create Trains with number of carriages less than zero' do
-    expect { train = Train.new('passenger', -4) }.to raise_error(ArgumentError)
-  end
-  it 'should create only Trains of cargo and passenger types' do
-    expect { train = Train.new('passenger', 10) }.not_to raise_error
-    expect { train = Train.new('cargo', 10) }.not_to raise_error
-    expect { train = Train.new('Passenger', 10) }.to raise_error(ArgumentError)
-    expect { train = Train.new(10, 10) }.to raise_error(ArgumentError)
-    expect { train = Train.new('personal', 10) }.to raise_error(ArgumentError)
-    expect { train = Train.new(nil, 10) }.to raise_error(ArgumentError)
+
+  context '#speed' do
+    before(:all) do
+      @train = Train.new('cargo', 10)
+    end
+    it 'should increase current speed by number' do
+      expect(@train.increase_speed_by(10)).to eq(10)
+      expect(@train.increase_speed_by(80.5)).to eq(90.5)
+    end
+    it 'shouldn`t increase speed to more than 120km/h' do
+      expect(@train.increase_speed_by(500)).to eq(120)
+    end
+    it 'should decrease current speed by number' do
+      expect(@train.decrease_speed_by(50)).to eq(70)
+      expect(@train.decrease_speed_by(7)).to eq(63)
+    end
+    it 'shouldn`t decrease current speed to less than 0' do
+      expect(@train.decrease_speed_by(100)).to eq(0)
+    end
   end
 end
-
-# describe Train do
-#   it 'should create object' do
-#     train = Train('cargo', 10)
-#     train.train_type.should == 'cargo'
-#     train.train_number.length.should == 10
-#   end
-# end
-
 # train_1 = Train.new('cargo', 10)
 #
 # puts "Train number: #{train_1.train_number}"
