7ef7de0e00bcd59e97ead89e131f06cc786279f9
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-25 08:09:33 +0300

    sending trains code added

--- Station.rb
+++ Station.rb
@@ -4,6 +4,7 @@ class Station
   def initialize(station_name)
     @station_name = check_station_name(station_name)
     @trains_at_station = []
+    @train_numbers = []
   end
 
   def check_station_name(name)
@@ -16,11 +17,27 @@ class Station
 
   def train_arrived(new_train)
     @trains_at_station.push(new_train)
+    @train_numbers.push(new_train.train_number)
   end
 
   def trains_at_station_of_type(train_type)
 
   end
 
-  private :check_station_name
+  def send_train(train_number)
+    error_message = "There is no train with number '#{train_number}' at station"
+    raise ArgumentError, error_message unless train_at_station?(train_number)
+
+    @trains_at_station.delete_at(get_train_index_by(train_number))
+  end
+
+  def train_at_station?(train_number)
+    @train_numbers.include? train_number
+  end
+
+  def get_train_index_by(train_name)
+    @train_numbers.find_index(train_name)
+  end
+
+  private :check_station_name, :train_at_station?, :get_train_index_by
 end
