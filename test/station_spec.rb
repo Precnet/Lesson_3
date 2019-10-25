8c105febbbab832697937c90c9f922ca9094f1b1
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-25 07:57:14 +0300

    reworking tests

--- test/station_spec.rb
+++ test/station_spec.rb
@@ -17,16 +17,24 @@ describe 'Station' do
     before(:all) do
       @station = Station.new('some_station')
     end
+    it 'should send trains one by one' do
+
+    end
     it 'should add trains to station one by one' do
-      train_1 = double('Train', train_type: 'cargo')
-      train_2 = double('Train', train_type: 'passenger')
-      train_3 = double('Train', train_type: 'cargo')
+      train_1 = double('Train', train_type: 'cargo', train_number: '001')
+      train_2 = double('Train', train_type: 'passenger', train_number: '002')
+      train_3 = double('Train', train_type: 'cargo', train_number: '003')
       @station.train_arrived(train_1)
       expect(@station.trains_at_station.length).to eq(1)
       @station.train_arrived(train_2)
       @station.train_arrived(train_3)
       expect(@station.trains_at_station.length).to eq(3)
       expect(@station.trains_at_station[1].train_type).to eq('passenger')
+    end
+    it 'should return trains currently at station' do
+      # train = double('Train', train_type: 'cargo', train_number: '001')
+      # @station.train_arrived(train)
+      # expect(@station.trains_at_station).to eq([train])
     end
   end
 end
