532ad50eaa4760144c914c7802a3aa3bc81f95d6
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-25 07:25:23 +0300

    variable name changed

--- test/station_spec.rb
+++ test/station_spec.rb
@@ -4,6 +4,6 @@ require_relative '../Station.rb'
 describe 'Station' do
   it 'should create station with custom name' do
     station = Station.new('first_station_ever')
-    expect(station.name).to eq('first_station_ever')
+    expect(station.station_name).to eq('first_station_ever')
   end
 end
