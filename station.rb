ae5cedb12cc8ff6e1fd4d47229c08555d3afe105
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-25 21:00:39 +0300

    removed excess method, reformat code: private method declaration corrected

--- station.rb
+++ station.rb
@@ -3,9 +3,8 @@ class Station
   def initialize(station_name)
     @station_name = check_station_name(station_name)
     @trains_at_station = []
-    @train_numbers = []
   end
-  def check_station_name(name)
+  private def check_station_name(name)
     raise ArgumentError, 'Station name can`t be nil!' unless name
     raise ArgumentError, 'Station name should be of String class!' unless name.is_a?(String)
     raise ArgumentError, 'Station name can`t be empty!' unless name.length > 0
@@ -14,7 +13,6 @@ class Station
   end
   def train_arrived(new_train)
     @trains_at_station.push(new_train)
-    @train_numbers.push(new_train.train_number)
   end
   def trains_at_station_of_type(train_type)
     @trains_at_station.select {|train| train if train.train_type == train_type}.map { |train| train.train_number}
@@ -28,17 +26,13 @@ class Station
   def send_train(train_number)
     error_message = "There is no train with number '#{train_number}' at station"
     raise ArgumentError, error_message unless train_at_station?(train_number)
-
     train_index =  get_train_index_by(train_number)
     @trains_at_station.delete_at(train_index)
-    @train_numbers.delete_at(train_index)
   end
-  def train_at_station?(train_number)
-    @train_numbers.include? train_number
+  private def train_at_station?(train_number)
+    @trains_at_station.map { |train| train.train_number}.include? train_number
   end
-
-  def get_train_index_by(train_name)
-    @train_numbers.find_index(train_name)
+  private def get_train_index_by(train_name)
+    @trains_at_station.map { |train| train.train_number}.find_index(train_name)
   end
-  private :check_station_name, :train_at_station?, :get_train_index_by
 end
