06226f98debdb551cc3964ad6efb3fb14d33967d
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-25 06:49:44 +0300

    typo

--- test/route_spec.rb
+++ test/route_spec.rb
@@ -3,7 +3,7 @@ require_relative '../Route.rb'
 
 describe 'Route' do
   context 'initialize' do
-    it 'takes only two arguments and constructs array of initial stations' do
+    it 'takes two arguments and constructs array of initial stations' do
       route = Route.new('first', 'second')
       expect(route.stations.length).to eq(2)
       expect(route.stations[0]).to eq('first')
