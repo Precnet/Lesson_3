0622da259ce208afe64d691ae8843d76217a9dac
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-25 06:58:54 +0300

    added test for show_route

--- test/route_spec.rb
+++ test/route_spec.rb
@@ -2,12 +2,16 @@ require 'rspec'
 require_relative '../Route.rb'
 
 describe 'Route' do
-  context 'initialize' do
-    it 'takes two arguments and constructs array of initial stations' do
-      route = Route.new('first', 'second')
-      expect(route.stations.length).to eq(2)
-      expect(route.stations[0]).to eq('first')
-      expect(route.stations[1]).to eq('second')
-    end
+  before(:all) do
+    @route = Route.new('first', 'second')
+  end
+  it 'should take two arguments and constructs array of initial stations' do
+    expect(@route.stations.length).to eq(2)
+    expect(@route.stations[0]).to eq('first')
+    expect(@route.stations[1]).to eq('second')
+  end
+  it 'should correctly display string representation of route' do
+    expect { @route.show_route }.to output("Route: first => second\n").to_stdout
+    expect { @route.show_route }.not_to output("Route: second <= first\n").to_stdout
   end
 end
