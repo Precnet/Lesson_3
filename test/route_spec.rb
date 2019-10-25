6d9466fb68f087b28c04a6ba5d826ca0600327f5
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-25 07:01:23 +0300

    added method to test adding station

--- test/route_spec.rb
+++ test/route_spec.rb
@@ -3,15 +3,19 @@ require_relative '../Route.rb'
 
 describe 'Route' do
   before(:all) do
-    @route = Route.new('first', 'second')
+    @route = Route.new('first', 'last')
   end
   it 'should take two arguments and constructs array of initial stations' do
     expect(@route.stations.length).to eq(2)
     expect(@route.stations[0]).to eq('first')
-    expect(@route.stations[1]).to eq('second')
+    expect(@route.stations[1]).to eq('last')
   end
   it 'should correctly display string representation of route' do
-    expect { @route.show_route }.to output("Route: first => second\n").to_stdout
-    expect { @route.show_route }.not_to output("Route: second <= first\n").to_stdout
+    expect { @route.show_route }.to output("Route: first => last\n").to_stdout
+    expect { @route.show_route }.not_to output("Route: last <= first\n").to_stdout
+  end
+  it 'should add intermediate station to route' do
+    @route.add_station('new_station')
+    expect(@route.stations[1]).to eq('new_station')
   end
 end
