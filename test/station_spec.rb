5e97cc5e751adcb5adda4ebd5ee6239f9dcf5c43
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-25 07:27:48 +0300

    added tests for incorrect station names

--- test/station_spec.rb
+++ test/station_spec.rb
@@ -6,4 +6,11 @@ describe 'Station' do
     station = Station.new('first_station_ever')
     expect(station.station_name).to eq('first_station_ever')
   end
+  it 'shouldn`t create station with incorrect name' do
+    expect { Station.new(nil) }.to raise_error(ArgumentError)
+    expect { Station.new(2452342) }.to raise_error(ArgumentError)
+    expect { Station.new(['good_station_name']) }.to raise_error(ArgumentError)
+    expect { Station.new('') }.to raise_error(ArgumentError)
+    expect { Station.new('very-very-very long station name') }.to raise_error(ArgumentError)
+  end
 end
