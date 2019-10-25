c4fe9b250a84b99b68944d624e305d8666b5bcbd
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-25 07:24:07 +0300

    added init name, and some checks for it

--- Station.rb
+++ Station.rb
@@ -1,3 +1,15 @@
 class Station
+  def initialize(station_name)
+    @station_name = check_station_name(station_name)
+  end
 
+  def check_station_name(name)
+    raise ArgumentError, 'Station name can`t be nil!' unless name
+    raise ArgumentError, 'Station name should be of String class!' unless name.is_a?(String)
+    raise ArgumentError, 'Station name can`t be empty!' unless name.length > 0
+    raise ArgumentError, 'Station name is too long! Should be <= 20 symbols.' unless name.length <= 20
+    name
+  end
+
+  private :check_station_name
 end
