2f41f2cd2e67a824f21762b30bcb73a907cf2f1a
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-25 19:53:51 +0300

    removed empty lines

--- route.rb
+++ route.rb
@@ -1,24 +1,19 @@
 class Route
   attr_reader :stations
-
   def initialize(first_station, last_station)
     @stations = [first_station, last_station]
   end
-
   def show_route
     puts 'Route: ' + @stations.join(' => ')
   end
-
   def add_station(new_station)
     # check for duplication
     duplicate_station_message = 'Can`t add duplicate stations to the route!'
     raise ArgumentError, duplicate_station_message if @stations.find_index(new_station)
     @stations.insert(-2, new_station)
   end
-
   def delete_station(station)
     raise ArgumentError, "There is no station #{station} in current route!" unless @stations.include? station
     @stations.delete_at(@stations.find_index(station))
   end
-
 end
