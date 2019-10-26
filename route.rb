6d00c94fa70902a00f4984e18a8aca956d91743b
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-26 07:03:01 +0300

    added empty lines between methods

--- route.rb
+++ route.rb
@@ -4,12 +4,14 @@ class Route
   def initialize(first_station, last_station)
     @stations = [first_station, last_station]
   end
+
   def add_station(new_station)
     # check for duplication
     duplicate_station_message = 'Can`t add duplicate stations to the route!'
     raise ArgumentError, duplicate_station_message if @stations.find_index(new_station)
     @stations.insert(-2, new_station)
   end
+
   def delete_station(station)
     raise ArgumentError, "There is no station #{station} in current route!" unless @stations.include? station
     @stations.delete_at(@stations.find_index(station))
