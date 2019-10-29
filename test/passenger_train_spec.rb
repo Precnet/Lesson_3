c56c196e9689d204a3d79a8132ef8e65ea0c1511
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-29 07:15:47 +0300

    added tests

--- test/passenger_train_spec.rb
+++ test/passenger_train_spec.rb
@@ -8,5 +8,20 @@ describe 'PassengerTrain' do
     expect(train.train_type).not_to eq('cargo')
     expect(train.train_type).not_to eq('12345')
     expect(train.class).to eq(PassengerTrain)
+    expect(train.carriages.length).to eq(0)
+  end
+
+  context 'Carriage manipulations' do
+    before(:all) do
+      @train = PassengerTrain.new
+    end
+    it 'should add new carriages' do
+      carriage = double('PassengerCarriage', carriage_type: 'passenger')
+      @train.add_carriage(carriage)
+      expect(@train.carriages.length).to eq(1)
+      @train.add_carriage(carriage)
+      expect(@train.carriages.length).to eq(2)
+      expect { @train.add_carriage('passenger') }.to raise_error(ArgumentError)
+    end
   end
 end
