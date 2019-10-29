c436d15aea59731e2005ccfa9040b20d0c47e468
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-29 07:01:55 +0300

    added method add_carriage

--- passenger_train.rb
+++ passenger_train.rb
@@ -7,4 +7,17 @@ class PassengerTrain < Train
     super(train_type='passenger', number_of_carriages=0)
     @carriages = []
   end
+
+  def add_carriage(carriage)
+    error_message = "Wrong carriage for this type of train. Expected 'PassengerCarriage', got #{carriage.class}."
+    raise ArgumentError, error_message unless carriage_correct?(carriage)
+    carriages.add(carriage)
+  end
+
+  private
+
+  def carriage_correct?(carriage)
+    carriage.carriage_type == 'passenger'
+  end
+
 end
