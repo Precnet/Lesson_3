6ab28100d8440d47460ffc4ff5c8110af0a2ce14
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-29 06:43:40 +0300

    file for tests added

--- test/passenger_train_spec.rb
+++ test/passenger_train_spec.rb
@@ -1,9 +1,17 @@
 require 'rspec'
-require_relative '../passenger_carriage.rb'
 
-describe 'PassengerCarriage' do
-  it 'should create passenger carriage' do
-    carriage = PassengerCarriage.new
-    expect(carriage.carriage_type).to eq('passenger')
+describe 'PassengerTrain' do
+  before do
+    # Do nothing
+  end
+
+  after do
+    # Do nothing
+  end
+
+  context 'when condition' do
+    it 'succeeds' do
+      pending 'Not implemented'
+    end
   end
 end
