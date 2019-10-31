f01465a8167c949af21afb05da966ff6f1f999ef
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-31 06:37:25 +0300

    cargo train

--- cargo_train.rb
+++ cargo_train.rb
@@ -1,16 +1,16 @@
 require_relative 'train.rb'
 
-class PassengerTrain < Train
+class CargoTrain < Train
   attr_reader :carriages
 
   def initialize
-    super(train_type='passenger', number_of_carriages=0)
+    super(train_type='cargo', number_of_carriages=0)
     @carriages = []
   end
 
   def add_carriage(carriage)
     raise RuntimeError, 'Can`t add new carriages while train is moving.' unless @current_speed == 0
-    error_message = "Wrong carriage for this type of train. Expected 'PassengerCarriage', got #{carriage.class}."
+    error_message = "Wrong carriage for this type of train. Expected 'CargoCarriage', got #{carriage.class}."
     raise ArgumentError, error_message unless carriage_correct?(carriage)
     carriages.push(carriage)
   end
@@ -24,7 +24,7 @@ class PassengerTrain < Train
   private
 
   def carriage_correct?(carriage)
-    carriage.respond_to?(:carriage_type) && carriage.carriage_type == 'passenger'
+    carriage.respond_to?(:carriage_type) && carriage.carriage_type == 'cargo'
   end
 
 end
