bf201baf500b94fb66d486bbb2661714f839bc2e
Dmitry Davdov <dmfrodol@yandex.ru>
2019-10-31 07:33:44 +0300

    basic functionality of UI class

--- main.rb
+++ main.rb
@@ -1,18 +1,30 @@
 class UserInterface
 
   def initialize
+    @menu_items = {}
     @stations = []
   end
 
-  private
+  def create_menu_item(item, command)
+    @menu_items[item] = command
+  end
+
+  def select_menu_item(item)
+    raise ArgumentError, 'No such menu item!' unless @menu_items.keys.include?(item)
+    @menu_items[item].call
+  end
 
   def create_station(station_name)
     station = Station.new(station_name)
     @stations.push(station)
+    puts "Created station: #{station_name}"
   end
 
-  def show_existing_stations
+  private
 
+  def show_existing_stations
+    puts 'There are next stations:'
+    puts ', '.join(@stations)
   end
 
 end
--- test/main_spec.rb
+++ test/main_spec.rb
@@ -1,6 +1,12 @@
 require 'rspec'
 require_relative '../main.rb'
 
-describe 'main' do
-
+describe 'UserInterface' do
+  context 'creating and selecting new menu items' do
+    it 'should add new stations' do
+      ui = UserInterface.new
+      ui.create_menu_item('Create new station', lambda { ui.create_station('test') })
+      expect { ui.select_menu_item('Create new station') }.to output("Created station: test\n").to_stdout
+    end
+  end
 end
