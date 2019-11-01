60bd37dd0e70c0a2679aa8546c87311c772ccb52
Dmitry Davdov <dmfrodol@yandex.ru>
2019-11-01 21:15:06 +0300

    implemeting tests

--- test/main_spec.rb
+++ test/main_spec.rb
@@ -8,5 +8,11 @@ describe 'UserInterface' do
       ui.create_menu_item('Create new station', lambda { ui.create_station('test') })
       expect { ui.select_menu_item('Create new station') }.to output("Created station: test\n").to_stdout
     end
+    it 'should show all created stations' do
+      ui = UserInterface.new
+      expect { ui.show_existing_stations }.to output("There are no stations.\n").to_stdout
+      ui.create_station('one')
+      expect { ui.show_existing_stations }.to output("There are next stations:\none\n").to_stdout
+    end
   end
 end
