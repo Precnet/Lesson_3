c2a9a50187e932e355341a70dd555bd1c6b73379
dmitiydavidov <dmfrodol@yandex.ru>
2019-11-03 12:12:34 +0300

    rewrited tests according to new lambda functionality

--- test/main_spec.rb
+++ test/main_spec.rb
@@ -3,19 +3,22 @@ require_relative '../main.rb'
 
 describe 'UserInterface' do
   context 'creating and selecting new menu items' do
+    before(:each) do
+      @ui = UserInterface.new
+      @ua = UserActions.new
+      @ui.create_menu_item('Show existing stations', lambda { @ua.show_existing_stations })
+      @ui.create_menu_item('Create new station', -> (station) { @ua.create_station station})
+    end
     it 'should add new stations' do
-      ui = UserInterface.new
-      ui.create_menu_item('Create new station', lambda { ui.create_station('test') })
-      expect { ui.select_menu_item('Create new station') }.to output("Created station: test\n").to_stdout
+      expect { @ui.select_menu_item('Create new station', 'test') }.to output("Created station: test\n").to_stdout
     end
     it 'should show all created stations' do
-      ui = UserInterface.new
-      expect { ui.show_existing_stations }.to output("There are no stations.\n").to_stdout
-      ui.create_station('one')
-      expect { ui.show_existing_stations }.to output("There are next stations:\none\n").to_stdout
-      ui.create_station('two')
-      ui.create_station('three')
-      expect { ui.show_existing_stations }.to output("There are next stations:\none, two, three\n").to_stdout
+      expect { @ui.select_menu_item('Show existing stations') }.to output("There are no stations.\n").to_stdout
+      @ui.select_menu_item('Create new station', 'one')
+      expect { @ui.select_menu_item('Show existing stations') }.to output("There are next stations:\none\n").to_stdout
+      @ui.select_menu_item('Create new station', 'two')
+      @ui.select_menu_item('Create new station', 'three')
+      expect { @ui.select_menu_item('Show existing stations') }.to output("There are next stations:\none, two, three\n").to_stdout
     end
   end
 end
