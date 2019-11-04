fa6859490b3c6586410cb8203bb677ea193f4ada
dmitiydavidov <dmfrodol@yandex.ru>
2019-11-04 19:03:10 +0300

    testing new routes

--- test/main_spec.rb
+++ test/main_spec.rb
@@ -5,20 +5,25 @@ describe 'UserInterface' do
   context 'creating and selecting new menu items' do
     before(:each) do
       @ui = UserInterface.new
-      @ua = UserActions.new
+      @ud = UserData.new
+      @ua = UserActions.new(@ud)
       @ui.create_menu_item('Show existing stations', lambda { @ua.show_existing_stations })
       @ui.create_menu_item('Create new station', -> (station) { @ua.create_station station})
     end
     it 'should add new stations' do
-      expect { @ui.select_menu_item('Create new station', 'test') }.to output("Created station: test\n").to_stdout
+      message = "Created station: test\n"
+      expect { @ui.select_menu_item('Create new station', 'test') }.to output(message).to_stdout
     end
     it 'should show all created stations' do
-      expect { @ui.select_menu_item('Show existing stations') }.to output("There are no stations.\n").to_stdout
+      message_1 = "There are no stations.\n"
+      expect { @ui.select_menu_item('Show existing stations') }.to output(message_1).to_stdout
       @ui.select_menu_item('Create new station', 'one')
-      expect { @ui.select_menu_item('Show existing stations') }.to output("There are next stations:\none\n").to_stdout
+      message_2 = "There are next stations:\none\n"
+      expect { @ui.select_menu_item('Show existing stations') }.to output(message_2).to_stdout
       @ui.select_menu_item('Create new station', 'two')
       @ui.select_menu_item('Create new station', 'three')
-      expect { @ui.select_menu_item('Show existing stations') }.to output("There are next stations:\none, two, three\n").to_stdout
+      message_3 = "There are next stations:\none, two, three\n"
+      expect { @ui.select_menu_item('Show existing stations') }.to output(message_3).to_stdout
     end
     it 'should create new trains' do
       @ui.create_menu_item('Create new passenger train', -> (number=nil) { @ua.create_passenger_train number})
@@ -33,6 +38,13 @@ describe 'UserInterface' do
       @ui.select_menu_item('Show existing trains')
       message_3 = "There are next passenger trains: test\nThere are next cargo trains: 1234,4321\n"
       expect { @ui.select_menu_item('Show existing trains') }.to output(message_3).to_stdout
+    end
+    it 'should create new routes' do
+      @ui.select_menu_item('Create new station', 'first')
+      @ui.select_menu_item('Create new station', 'last')
+      @ui.create_menu_item('Create new route', -> (first, last, number=nil) {@ua.create_route(first, last, number)})
+      message = "Route 'test' created\n"
+      expect { @ui.select_menu_item('Create new route', [@ud.stations['first'], @ud.stations['last'], 'test']) }.to output(message).to_stdout
     end
   end
 end
