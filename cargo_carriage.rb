300545873d19f6268ed401465290c89660986f9a
dmitiydavidov <dmfrodol@yandex.ru>
2019-11-05 11:18:53 +0300

    added constant for carriage type

--- cargo_carriage.rb
+++ cargo_carriage.rb
@@ -1,16 +1,17 @@
 class CargoCarriage
   NUMBER_LENGTH = 5
   BASE_36 = 36
+  CARRIAGE_TYPE = 'cargo'
 
   attr_reader :carriage_type, :carriage_number
 
   def initialize(carriage_number=generate_carriage_number(NUMBER_LENGTH))
-    @carriage_type = 'cargo'
+    @carriage_type = CARRIAGE_TYPE
     @carriage_number = carriage_number
   end
 
   private
   def generate_carriage_number(number_length)
-    @carriage_type + '_' + rand(BASE_36 ** number_length).to_s(BASE_36)
+    CARRIAGE_TYPE + '_' + rand(BASE_36 ** number_length).to_s(BASE_36)
   end
 end
