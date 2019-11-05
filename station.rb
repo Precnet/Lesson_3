ed2d9749e4c93d9662cb434b30250cbfdc642047
dmitiydavidov <dmfrodol@yandex.ru>
2019-11-05 20:13:37 +0300

    moved private methods to private sections

--- station.rb
+++ station.rb
@@ -6,16 +6,15 @@ class Station
     @trains_at_station = []
   end
 
-  private def check_station_name(name)
-    raise ArgumentError, 'Station name can`t be nil!' unless name
-    raise ArgumentError, 'Station name should be of String class!' unless name.is_a?(String)
-    raise ArgumentError, 'Station name can`t be empty!' unless name.length > 0
-    raise ArgumentError, 'Station name is too long! Should be <= 20 symbols.' unless name.length <= 20
-    name
-  end
-
   def train_arrived(new_train)
     @trains_at_station.push(new_train)
+  end
+
+  def send_train(train_number)
+    error_message = "There is no train with number '#{train_number}' at station"
+    raise ArgumentError, error_message unless train_at_station?(train_number)
+    train_index =  get_train_index_by(train_number)
+    @trains_at_station.delete_at(train_index)
   end
 
   def trains_at_station_of_type(train_type)
@@ -29,18 +28,20 @@ class Station
     result
   end
 
-  def send_train(train_number)
-    error_message = "There is no train with number '#{train_number}' at station"
-    raise ArgumentError, error_message unless train_at_station?(train_number)
-    train_index =  get_train_index_by(train_number)
-    @trains_at_station.delete_at(train_index)
+  private
+  def check_station_name(name)
+    raise ArgumentError, 'Station name can`t be nil!' unless name
+    raise ArgumentError, 'Station name should be of String class!' unless name.is_a?(String)
+    raise ArgumentError, 'Station name can`t be empty!' unless name.length > 0
+    raise ArgumentError, 'Station name is too long! Should be <= 20 symbols.' unless name.length <= 20
+    name
   end
 
-  private def train_at_station?(train_number)
+  def train_at_station?(train_number)
     @trains_at_station.map { |train| train.train_number}.include? train_number
   end
 
-  private def get_train_index_by(train_name)
+  def get_train_index_by(train_name)
     @trains_at_station.map { |train| train.train_number}.find_index(train_name)
   end
 end
