b81c3873dca66c7b8cb8f8ff6a0e2a1fcd17e99d
dmitiydavidov <dmfrodol@yandex.ru>
2019-11-05 11:16:13 +0300

    added constants added random carriage name

--- cargo_carriage.rb
+++ cargo_carriage.rb
@@ -1,7 +1,16 @@
 class CargoCarriage
-  attr_reader :carriage_type
+  NUMBER_LENGTH = 5
+  BASE_36 = 36
 
-  def initialize
+  attr_reader :carriage_type, :carriage_number
+
+  def initialize(carriage_number=generate_carriage_number(NUMBER_LENGTH))
     @carriage_type = 'cargo'
+    @carriage_number = carriage_number
+  end
+
+  private
+  def generate_carriage_number(number_length)
+    @carriage_type + '_' + rand(BASE_36 ** number_length).to_s(BASE_36)
   end
 end
